7abdc7716f4e680fed8b397a9b53406b4f44c38f
Muhammad Rafli K P W <141980262+Raflikr@users.noreply.github.com>
2023-12-02 09:40:29 +0700

    Tugas pertemuan tanggal 30 Nop

--- resources/views/edit.blade.php
+++ resources/views/edit.blade.php
@@ -1,29 +1,53 @@
-<!DOCTYPE html>
-<html>
-<head>
-	<title>Tutorial Membuat CRUD Pada Laravel - www.malasngoding.com</title>
-</head>
-<body>
+@extends('master2')
+@section('judulhalaman','Data Pegawai')
 
-	<h2><a href="https://www.malasngoding.com">www.malasngoding.com</a></h2>
-	<h3>Edit Pegawai</h3>
+@section('konten')
+  <h2><a href="https://www.malasngoding.com">PT. Sumber Makmur Persero Tbk.</a></h2>
+  <h3>Edit Pegawai</h3>
 
-	<a href="/pegawai"> Kembali</a>
+  <a href="/pegawai"> Kembali</a>
 
-	<br/>
-	<br/>
+  <br/>
+  <br/>
 
-	@foreach($pegawai as $p)
-	<form action="/pegawai/update" method="post">
-		{{ csrf_field() }}
-		<input type="hidden" name="id" value="{{ $p->pegawai_id }}"> <br/>
-		Nama <input type="text" required="required" name="nama" value="{{ $p->pegawai_nama }}"> <br/>
-		Jabatan <input type="text" required="required" name="jabatan" value="{{ $p->pegawai_jabatan }}"> <br/>
-		Umur <input type="number" required="required" name="umur" value="{{ $p->pegawai_umur }}"> <br/>
-		Alamat <textarea required="required" name="alamat">{{ $p->pegawai_alamat }}</textarea> <br/>
-		<input type="submit" value="Simpan Data">
-	</form>
-	@endforeach
+  @foreach($pegawai as $p)
+  <form action="/pegawai/update" method="post">
+    {{ csrf_field() }}
+    <input type="hidden" name="id" value="{{ $p->pegawai_id }}"> <br/>
+    <div class="form-group row">
+        <label for="nama" class="col-sm-1 col-form-label mr-2">Nama</label>
+        <div class="col-sm-4">
+            <input type="text" required="required" class="form-control" name="nama" value="{{ $p->pegawai_nama }}">
+        </div>
+    </div>
 
-</body>
-</html>
+    <div class="form-group row">
+        <label for="jabatan" class="col-sm-1 col-form-label mr-2">Jabatan</label>
+        <div class="col-sm-4">
+            <input type="text" required="required" class="form-control" name="jabatan" value="{{ $p->pegawai_jabatan }}">
+        </div>
+    </div>
+
+    <div class="form-group row">
+        <label for="umur" class="col-sm-1 col-form-label mr-2">Umur</label>
+        <div class="col-sm-4">
+            <input type="number" required="required" class="form-control" name="umur" value="{{ $p->pegawai_umur }}">
+        </div>
+    </div>
+
+    <div class="form-group row">
+        <label for="alamat" class="col-sm-1 col-form-label mr-2">Alamat</label>
+        <div class="col-sm-4">
+            <textarea required="required" class="form-control" name="alamat"  rows="8">{{ $p->pegawai_alamat }}</textarea>
+        </div>
+    </div>
+
+    <div class="form-group row">
+        <div class="col-sm-9 offset-sm-1">
+            <input type="submit" class="btn btn-primary" value="Simpan Data">
+        </div>
+    </div>
+  </form>
+  @endforeach
+
+@endsection
--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -2,19 +2,24 @@
 @section('judulhalaman','Data Pegawai')
 
 @section('konten')
-	<h2>www.malasngoding.com</h2>
+	<h2>PT. Sumber Makmur Persero Tbk.</h2>
 	<h3>Data Pegawai</h3>
 
-	<a href="/pegawai/tambah" class="btn btn-primary"> + Tambah Pegawai Baru</a>
+
+	<a href="/pegawai/tambah" class="btn btn-primary mb-3 mt-2" > + Tambah Pegawai Baru</a>
 
 	<br/>
     <p>Cari Data Pegawai :</p>
-	<form action="/pegawai/cari" method="GET">
-		<input class="form-control" type="text" name="cari" placeholder="Cari Nama Pegawai .."
-        value="{{ old('cari', isset($cari) ? $cari : '') }}">
-		<input type="submit" value="CARI" class="btn btn-info">
-	</form>
-	<br/>
+    <form action="/pegawai/cari" method="GET" class="mb-3">
+        <div class="input-group">
+            <input type="text" class="form-control" name="cari" placeholder="Cari Nama Pegawai"
+                value="{{ old('cari', isset($cari) ? $cari : '') }}">
+            <div class="input-group-append">
+                <button class="btn btn-info" type="submit">CARI</button>
+            </div>
+        </div>
+    </form>
+    <br/>
 
 	<table class="table table-striped table-hover">
 		<tr>
@@ -31,6 +36,8 @@
 			<td>{{ $p->pegawai_umur }}</td>
 			<td>{{ $p->pegawai_alamat }}</td>
 			<td>
+                <a href="/pegawai/view/{{ $p->pegawai_id }}" class="btn btn-success">View</a>
+                |
 				<a href="/pegawai/edit/{{ $p->pegawai_id }}" class="btn btn-warning">Edit</a>
 				|
 				<a href="/pegawai/hapus/{{ $p->pegawai_id }}" class="btn btn-danger">Hapus</a>
